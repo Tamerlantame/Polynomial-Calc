4962ae8e8662af5145465b038289c0774de808c1
Mikhail R. Starchak <mikhstark@gmail.com>
2020-09-04 17:23:01 +0300

    Task for Yassin in EuclidFunctions.

--- Arithmetics/EuclidFunctions.cs
+++ Arithmetics/EuclidFunctions.cs
@@ -9,17 +9,17 @@ namespace Arithmetics
     /// <summary>
     /// Static methods for Euclid's algorithm and its generalizations
     /// </summary>
-    static class EuclidFunctions
+    public static class EuclidFunctions
     {
         /// <summary>
-        /// Classic Euclid's algorithm for greatest common divisor
+        /// Classic Euclid's algorithm for the greatest common divisor function
         /// </summary>
         /// <param name="x">first number</param>
         /// <param name="y">second number</param>
         /// <returns>Returns the greatest common divisor of the two integers</returns>
         public static int Euclid(int x, int y)
         {
-            x = Math.Abs(x);
+            x = Math.Abs(x); // Убрать эту ерунду.
             y = Math.Abs(y);
             while (x != 0 && y != 0)
             {
@@ -34,17 +34,27 @@ namespace Arithmetics
             return x + y;
         }
 
+        /// <summary>
+        /// ????????????????
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
         public static int Euclid(int[] a)
         {
-            int gcd = Euclid(a[1], a[0]);
-            for (int i = 2; i < a.Length; i++)
-            {
+            int gcd = 0;
+            for (int i = 0; i < a.Length; i++)
                 gcd = Euclid(gcd, a[i]);
-                gcd = Euclid(gcd, a[i]);
-            }
             return gcd;
         }
 
+        /// <summary>
+        /// Всё напишите. Посмотрите, что возвращает метод. Должен быть НОД(x,y)
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public static int ExtendedEuclid(int x, int y, out int a, out int b)
         {
             int q, r;
@@ -58,10 +68,11 @@ namespace Arithmetics
                 r = x % y;
                 x = y;
                 y = r;
-                r = coeff_a;
 
+                r = coeff_a;
                 coeff_a = a - q * coeff_a;
                 a = r;
+
                 r = coeff_b;
                 coeff_b = b - q * coeff_b;
                 b = r;
@@ -70,49 +81,12 @@ namespace Arithmetics
         }
 
         // TODO Яссин, смотрите! Это нужно сделать аккуратнее.
-        public static int[] LinearDiophantine(int res, int[] coeffs, int length)
+        // (04.09) Точнее, просто всё перепишите. Сделайте так, как мы с вами делали эту задачу прошлой осенью.
+        public static int[] LinearDiophantine(int[] a, int b)
         {
-
-            int[] results = new int[length];
-            if (length == 2)
-            {
-                int nod = Euclid(coeffs[1], coeffs[0]);
-                if (Euclid(nod, res) != 1)
-                {
-                    coeffs[1] = coeffs[1] / Euclid(nod, res);
-                    coeffs[0] = coeffs[0] / Euclid(nod, res);
-
-                }
-                ExtendedEuclid(coeffs[0], coeffs[1], out results[0], out results[1]);
-
-                results[0] = results[0] * res;
-                results[1] = results[1] * res;
-
-
-                return results;
-            }
-            else
-            {
-                int nod = coeffs[0];
-                for (int i = 1; i < length - 1; i++)
-                    nod = Euclid(nod, coeffs[i]);
-
-                int nod_all = Euclid(nod, coeffs[length - 1]);
-                int L = res;
-                nod /= nod_all;
-                res /= nod_all;
-                coeffs[length - 1] = coeffs[length - 1] / nod_all;
-
-                ExtendedEuclid(nod, coeffs[length - 1], out int x, out results[length - 1]);
-                results[length - 1] *= res;
-                x *= L;
-                int[] temp = LinearDiophantine(x, coeffs, length - 1);
-                for (int i = 0; i < length - 1; i++)
-                {
-                    results[i] = temp[i];
-                }
-                return results;
-            }
+            int[] x = new int[a.Length];
+            //...
+            return x;
         }
     }
 }
