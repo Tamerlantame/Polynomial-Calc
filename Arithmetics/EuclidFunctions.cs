39e376dd1b4f438d4d6a246fc69b092f5663ac50
yassine zaoudi <zaoudiyassine@gmail.com>
2020-09-09 17:30:04 +0200

    - Added LinearDiiophantine - Added DeleteElement - Changed ExtendedEuclid - Changed comments

--- Arithmetics/EuclidFunctions.cs
+++ Arithmetics/EuclidFunctions.cs
@@ -6,87 +6,142 @@ using System.Threading.Tasks;
 
 namespace Arithmetics
 {
-    /// <summary>
-    /// Static methods for Euclid's algorithm and its generalizations
-    /// </summary>
+    /// <summary> 
+    /// Static methods for Euclid's algorithm and its generalizations 
+    /// </summary> 
     public static class EuclidFunctions
     {
-        /// <summary>
-        /// Classic Euclid's algorithm for the greatest common divisor function
-        /// </summary>
-        /// <param name="x">first number</param>
-        /// <param name="y">second number</param>
-        /// <returns>Returns the greatest common divisor of the two integers</returns>
+        /// <summary> 
+        /// Classic Euclid's algorithm for the greatest common divisor function 
+        /// </summary> 
+        /// <param name="x">First number</param> 
+        /// <param name="y">Second number</param> 
+        /// <returns>Returns the greatest common divisor of the two integers</returns> 
         public static int Euclid(int x, int y)
         {
-            x = Math.Abs(x); // Убрать эту ерунду.
-            y = Math.Abs(y);
             while (x != 0 && y != 0)
             {
                 if (x != 0 && y != 0)
                 {
                     if (x > y)
+                    {
                         x %= y;
+                    }
                     else
+                    {
                         y %= x;
+                    }
                 }
             }
             return x + y;
         }
 
-        /// <summary>
-        /// ????????????????
-        /// </summary>
-        /// <param name="a"></param>
-        /// <returns></returns>
-        public static int Euclid(int[] a)
+        /// <summary> 
+        /// Get the greatest common divisor of array elements. 
+        /// </summary> 
+        /// <param name="array">Array of integers</param> 
+        /// <returns>The greatest common divisor</returns> 
+        public static int Euclid(int[] array)
         {
             int gcd = 0;
-            for (int i = 0; i < a.Length; i++)
-                gcd = Euclid(gcd, a[i]);
+            for (int i = 0; i < array.Length; i++)
+            {
+                gcd = Euclid(gcd, array[i]);
+            }
             return gcd;
         }
 
-        /// <summary>
-        /// Всё напишите. Посмотрите, что возвращает метод. Должен быть НОД(x,y)
-        /// </summary>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        /// <returns></returns>
-        public static int ExtendedEuclid(int x, int y, out int a, out int b)
+        /// <summary> 
+        /// Get gcd(a, b) and find coeddicientA and coefficientB such that, a * coeddicientA + b * coefficientB = gsd(a, b). 
+        /// </summary> 
+        /// <param name="a">First unknown value</param> 
+        /// <param name="b">Second unknown value</param> 
+        /// <param name="coefficientA">Coefficient of first unknown value</param> 
+        /// <param name="coefficientB">Coefficient of second unknown value</param> 
+        /// <returns>gcd(a, b) —- The greatest common divisor of the two integers</returns> 
+        public static int ExtendedEuclid(int a, int b, out int coefficientA, out int coefficientB)
         {
-            int q, r;
-            a = 1;
-            int coeff_a = 0;
-            b = 0;
-            int coeff_b = 1;
-            while (y != 0)
+            if (a == 0)
             {
-                q = x / y;
-                r = x % y;
-                x = y;
-                y = r;
-
-                r = coeff_a;
-                coeff_a = a - q * coeff_a;
-                a = r;
-
-                r = coeff_b;
-                coeff_b = b - q * coeff_b;
-                b = r;
+                coefficientA = 0;
+                coefficientB = 1;
+                return b;
             }
-            return y;
+            int x1;
+            int y1;
+            int gcd = ExtendedEuclid(b % a, a, out x1, out y1);
+            coefficientA = y1 - (b / a) * x1;
+            coefficientB = x1;
+            return gcd;
         }
 
-        // TODO Яссин, смотрите! Это нужно сделать аккуратнее.
-        // (04.09) Точнее, просто всё перепишите. Сделайте так, как мы с вами делали эту задачу прошлой осенью.
-        public static int[] LinearDiophantine(int[] a, int b)
+        /// <summary> 
+        /// Delete value from array. 
+        /// </summary> 
+        /// <param name="array">Array of integers</param> 
+        /// <param name="value">Value for delition from array</param> 
+        /// <returns></returns> 
+        public static int[] DeleteElement(int[] array, int value)
         {
-            int[] x = new int[a.Length];
-            //...
-            return x;
+            int[] results = new int[array.Length - 1];
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (i == value)
+                {
+                    i++;
+                }
+                results[i] = array[i];
+            }
+            return results;
+        }
+
+        /// <summary> 
+        /// Get one solution to an equation. 
+        /// </summary> 
+        /// <param name="coefficients">Array of coefficients for unknown values</param> 
+        /// <param name="value">Free value of equation</param> 
+        /// <returns>One solution of equation.</returns> 
+        public static int[] LinearDiophantine(int[] coefficients, int value)
+        {
+            int[] results = new int[coefficients.Length];
+            int gcd = Euclid(coefficients);
+            if (value % gcd == 0)
+            {
+                if (coefficients.Length == 2)
+                {
+                    coefficients[0] /= gcd;
+                    coefficients[1] /= gcd;
+                    value /= gcd;
+                    ExtendedEuclid(coefficients[0], coefficients[1], out results[0], out results[1]);
+                    results[0] = results[0] * value;
+                    results[1] = results[1] * value;
+                    return results;
+                }
+
+                gcd = coefficients[0];
+                for (int i = 1; i < coefficients.Length - 1; i++)
+                {
+                    gcd = Euclid(gcd, coefficients[i]);
+                }
+
+                int gcdGlobal = Euclid(gcd, coefficients[coefficients.Length - 1]);
+                int imaginaryValue = value;
+                gcd /= gcdGlobal;
+                value /= gcdGlobal;
+                coefficients[coefficients.Length - 1] /= gcdGlobal;
+                ExtendedEuclid(gcd, coefficients[coefficients.Length - 1], out int temp, out results[coefficients.Length - 1]);
+                results[coefficients.Length - 1] *= value;
+                temp *= imaginaryValue;
+                coefficients = DeleteElement(coefficients, coefficients.Length - 1);
+                int[] temp1 = LinearDiophantine(coefficients, temp);
+
+                for (int i = 0; i < results.Length - 1; i++)
+                {
+                    results[i] = temp1[i];
+                }
+                return results;
+            }
+            return null;
         }
     }
 }
