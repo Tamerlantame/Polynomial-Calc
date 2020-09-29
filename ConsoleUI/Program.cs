7aca94860e0349038335434e3209c275b8f380a2
profandrew <fhvgggh.ru@gmail.com>
2020-09-29 14:26:25 +0300

    добавил форму для удобной работы

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -4,28 +4,39 @@ using GraphTheory;
 using Arithmetics;
 using System.IO;
 using Arithmetics.Polynomial;
+using System.Windows.Forms;
+
 
 namespace ConsoleUI
 {
     class Program
     {
-        static void Main(string[] args)
+        [STAThread]
+        static void Main()
         {
-            SortedList<string, Graph> Graphs = new SortedList<string, Graph>();
-            LyaMelikSession session = new LyaMelikSession();
-            session.Start();
-            Console.ReadKey();
-         /*   SortedList<string, Polynomial> polynomialList = new SortedList<string, Polynomial>();
-            ConsoleKeyInfo keyInfo = new ConsoleKeyInfo();
-            while (keyInfo.Key != ConsoleKey.Enter)
-            {
-                Console.WriteLine("Введите имя полинома");
-                string name = Console.ReadLine();
-                Console.WriteLine("Введите Полином");
-                Polynomial polynomial = new Polynomial(Console.ReadLine());
-                polynomialList.Add(name, polynomial);
-               
-            }*/
-        }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new UIForm());
+                       
+
+
+
+
+        //  SortedList<string, Graph> Graphs = new SortedList<string, Graph>();
+        //LyaMelikSession session = new LyaMelikSession();
+        //session.Start();
+        //Console.ReadKey();
+        /*   SortedList<string, Polynomial> polynomialList = new SortedList<string, Polynomial>();
+           ConsoleKeyInfo keyInfo = new ConsoleKeyInfo();
+           while (keyInfo.Key != ConsoleKey.Enter)
+           {
+               Console.WriteLine("Введите имя полинома");
+               string name = Console.ReadLine();
+               Console.WriteLine("Введите Полином");
+               Polynomial polynomial = new Polynomial(Console.ReadLine());
+               polynomialList.Add(name, polynomial);
+
+           }*/
     }
+}
 }
---
+++ ConsoleUI/UIForm.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using GraphTheory;
+using Microsoft.VisualBasic;
+
+namespace ConsoleUI
+{
+    public partial class UIForm : Form
+
+    {
+        private SortedList<string, Graph> graphs;
+        private string activeGraph;
+        public UIForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            graphs = new SortedList<string, Graph>();
+
+        }
+
+        private void help_button_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            string[] text = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "Help.txt"));
+            foreach (string item in text)
+            {
+                richTextBox1.Text = richTextBox1.Text + item + "\n";
+            }
+
+        }
+
+        private void create_button_Click(object sender, EventArgs e)
+        {
+
+            string path = Interaction.InputBox("Введите путь к графу", "", "");
+
+            try
+            {
+                Arithmetics.Matrix.IntegerSquareMatrix graphMatrix = new Arithmetics.Matrix.IntegerSquareMatrix(path);
+                if (graphMatrix.columns == 0)
+                {
+                    return;
+                }
+                string name = Interaction.InputBox("Введите имя графа", "", "");
+                if (graphs.ContainsKey(name) == false)
+                {
+                    Graph NewGraph = new Graph(graphMatrix);
+                    graphs.Add(name, NewGraph);
+                    Console.WriteLine("Граф " + name + " успешно добавлен");
+                    activeGraph = name;
+                    richTextBox1.Text = graphs[activeGraph].ToString();
+                    graphs_ToolStripMenuItem.DropDownItems.Add(name);
+
+                }
+                else
+                {
+                    Console.WriteLine("это имя уже занято");
+                }
+            }
+            catch
+            {
+                dialogue_value.Text = "Вы ввели что-то не то";
+                return;
+            }
+
+
+        }
+
+        private void graphs_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void transpose_button_Click(object sender, EventArgs e)
+        {
+            graphs[activeGraph] = graphs[activeGraph].Transponse();
+            richTextBox1.Text = graphs[activeGraph].ToString();
+        }
+
+        private void isbiparted_button_Click(object sender, EventArgs e)
+        {
+            if (graphs.Count != 0)
+            {
+                var check = graphs[activeGraph].IsBipartite();
+                if (check)
+                {
+                    MessageBox.Show("Этот граф двудолен");
+                }
+                else
+                {
+                    MessageBox.Show("Этот граф недвудолен");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Вы еще не ввели граф");
+            }
+        }
+
+        private void delete_button_Click(object sender, EventArgs e)
+        {for (int i=0; i<graphs_ToolStripMenuItem.DropDownItems.Count;i++)
+            {
+                if (graphs_ToolStripMenuItem.DropDownItems[i].Text == activeGraph) graphs_ToolStripMenuItem.DropDownItems.RemoveAt(i);
+            }
+                   
+            menuStrip1.Items.Remove(menuStrip1.Items[activeGraph]);
+            graphs.Remove(activeGraph);
+            activeGraph = null;
+            richTextBox1.Text = "";
+        }
+
+        private void graphs_ToolStripMenuItem_DropDownItemClicked_1(object sender, ToolStripItemClickedEventArgs e)
+        {
+            string name = e.ClickedItem.Text;
+            activeGraph = name;
+            richTextBox1.Text = graphs[name].ToString();
+        }
+    }
+}
+
+
