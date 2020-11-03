7d7732566612ad3182366288bc3f78f1f278ac66
Temur Melikov <temur.melikov@yandex.ru>
2020-11-03 14:23:06 +0300

    Промежкточное храниние Culculstor и PolynomialSession

---
+++ Arithmetics/Parsers/Сulculator.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Arithmetics.Parsers;
+
+namespace Arithmetics
+{
+    public class Сulculator
+    {
+
+        /// <summary>
+        /// Парсит строку в обратную польскую запись
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        public static string ExpressionToRPN(string expression)
+        {
+            var text = expression;
+            var reader = new StringReader(text);
+            var parser = new Parser();
+            var tokens = parser.Tokenize(reader).ToList();
+            var rpn = parser.ShuntingYard(tokens);
+            var rpnStr = string.Join(" ", rpn.Select(t => t.Value));
+            return rpnStr;
+        }
+        /// <summary>
+        /// вычисляет выражение, записанное в RPN(обратная польская запись)
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        public static string RPNtoAnswer(string expression)
+        {
+            var text = expression;
+            var reader = new StringReader(text);
+            var parser = new Parser();
+            var tokens = parser.Tokenize(reader).ToList().ToArray();
+            var stack = new Stack<Token>();
+            Token rightOp;
+            Token leftOp;
+            double result;
+            for(int i = 0; i < tokens.Length; i++)
+            {
+                switch (tokens[i].Type)
+                {
+                    case TokenType.Number:
+                        stack.Push(tokens[i]);
+                        break;
+                    case TokenType.Variable:
+                        stack.Push(tokens[i]);
+                        break;
+                    //case TokenType.Function:
+                    //    stack.Push(tok);
+                    //    break;
+                    case TokenType.Operator:
+                        rightOp = stack.Pop();
+                        leftOp = stack.Pop();
+
+                        // если в стеке храняться 2 числа
+
+                        if (leftOp.Type == TokenType.Number && rightOp.Type == TokenType.Number)
+                        {
+                            switch (tokens[i].Value)
+                            {
+
+                                case "^":
+                                    result = Math.Pow(Convert.ToDouble(leftOp.Value), Convert.ToDouble(rightOp.Value));
+                                    stack.Push(new Token(TokenType.Number, result.ToString()));
+                                    break;
+                                case "*":
+                                    result = Convert.ToDouble(leftOp.Value) * Convert.ToDouble(rightOp.Value);
+                                    stack.Push(new Token(TokenType.Number, result.ToString()));
+                                    break;
+                                case "/":
+                                    result = Convert.ToDouble(leftOp.Value) / Convert.ToDouble(rightOp.Value);
+                                    stack.Push(new Token(TokenType.Number, result.ToString()));
+                                    break;
+                                case "+":
+                                    result = Convert.ToDouble(leftOp.Value) + Convert.ToDouble(rightOp.Value);
+                                    stack.Push(new Token(TokenType.Number, result.ToString()));
+                                    break;
+                                case "-":
+                                    result = Convert.ToDouble(leftOp.Value) - Convert.ToDouble(rightOp.Value);
+                                    stack.Push(new Token(TokenType.Number, result.ToString()));
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                        break;
+
+                    default:
+                        throw new Exception("Wrong token");
+                }
+            }
+            text = stack.Pop().Value;
+
+            return text;
+        }
+    }
+}
---
+++ ConsoleUI/Sessions/PolynomialSession.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Arithmetics.Polynomial1;
+using Arithmetics.Parsers;
+using Arithmetics;
+using System.IO;
+
+
+namespace WinFormsUI.Sessions
+{
+    class PolynomialSession : Session
+    {
+        public PolynomialSession(RichTextBox input, RichTextBox output) : base(input, output)
+        {
+
+        }
+        public void Start()
+        {
+            inputRichTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(Execute);
+            //outputRichTextBox.Text += $"{Name} - session Execution: \n";
+        }
+        /// <summary>
+        /// Вычисляет последнюю строчку программы, если нажали Enter. 
+        /// </summary>
+        private void Execute(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (inputRichTextBox.SelectedText != "")
+                {
+                    //Вычисляем выделенную часть "кода"
+                    var text = inputRichTextBox.SelectedText;
+                    using (var reader = new StringReader(text))
+                    {
+                        var parser = new Parser();
+                        var tokens = parser.Tokenize(reader).ToList();
+                        var rpn = parser.ShuntingYard(tokens);
+                        outputRichTextBox.Text = string.Join(" ", rpn.Select(t => t.Value));
+                        //Методы из Arithmetics.Parsers Shunting-Yard algorithm
+                    }
+                }
+                else
+                {
+                    var text = inputRichTextBox.Lines.Last();
+                    outputRichTextBox.Text = Сulculator.ExpressionToRPN(text);
+                }
+            }
+
+        }
+    }
+}
